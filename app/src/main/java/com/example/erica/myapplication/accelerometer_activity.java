3b719db4dd58a9d1a63c39c97638225d1c77a903
Kemey <erica.karlsson@svartavingar.se>
2017-03-27 19:52:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/erica/myapplication/accelerometer_activity.java
@@ -0,0 +1,13 @@
+package com.example.erica.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class accelerometer_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accelerometer_activity);
+    }
+}
---
+++ app/src/main/java/com/example/erica/myapplication/compass_activity.java
@@ -0,0 +1,13 @@
+package com.example.erica.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class compass_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass_activity);
+    }
+}
